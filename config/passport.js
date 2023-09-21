7fb6d88ce4d7ae7937ce11c82057e2a4ff72ba7e
Mario <mariokozul24@gmail.com>
2023-09-21 20:22:27 +0200

    added functions for logging in user, serializeing and deserializeing

---
+++ config/passport.js
@@ -0,0 +1,40 @@
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { connection } from './database.js';
+import { User } from '../models/users.js';
+import { validPassword } from '../lib/passwordUtils.js';
+
+
+const verifyCallback = async (username, password, cb) => {
+    try {
+        const user = await User.findOne({username: username});
+        if(!user) return cb(null, false);
+        console.log(user);
+        console.log(password);
+        const isValid = validPassword(password, user.hash, user.salt);
+        console.log(isValid);
+
+        if(isValid) return cb(null, user);
+        else return cb(null, false);
+
+    } catch(err) {
+        cb(err);
+    }
+}
+
+const strategy = new LocalStrategy(verifyCallback);
+
+passport.use(strategy);
+
+passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+})
+
+passport.deserializeUser(async (userId, cb) => {
+    try {
+        const user = await User.findById(userId)
+        cb(null, user);
+    } catch(err) {
+        cb(err);
+    }
+})
