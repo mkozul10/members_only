cd52a35727a98ad8c485eab46d300479be10b828
Mario <mariokozul24@gmail.com>
2023-09-21 20:29:02 +0200

    implemented setup for sessions and passport

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ import mongoose from 'mongoose';
 import { config } from 'dotenv';
 import { connectDB } from './config/database.js';
 import memberRoutes from './routes/members.js';
+import passport from 'passport';
+import session from 'express-session';
+import { connection } from './config/database.js';
+import MongoStore from 'connect-mongo';
+
+import './config/passport.js'; 
 
 
 //init app, parsers, & env variables
@@ -36,6 +42,37 @@ app.use(morgan('dev'));
 //parsers
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+
+//session setup
+const mongoStore = MongoStore.create({
+    mongoUrl: process.env.DB_STRING, 
+    collectionName: 'sessions', 
+    mongooseConnection: connection
+});
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false, 
+    store:mongoStore,    
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}))
+
+//passport auth
+
+import './config/passport.js';
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req,res,next) => {
+    console.log(req.session);
+    console.log(req.user);
+    next();
+})
+
 
 app.get('/', (req, res) => {
     res.render('index');
