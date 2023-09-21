bbe1cdcb6c58b59ea355e9907b2994532d545a25
Mario <mariokozul24@gmail.com>
2023-09-21 20:25:43 +0200

    added middlleware for checking is username avaliable and double check for password input

---
+++ routes/middlleware.js
@@ -0,0 +1,21 @@
+import { User } from '../models/users.js';
+
+export const checkUsername = async (req,res,next) => {
+    try{
+        const user = await User.findOne({username: req.body.username})
+        if(user) res.render('members/sign-up',{err: true, msg: 'Username is already in use'});
+        else {
+            console.log(req.body);
+            next();
+        }
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+export const checkPassword = (req,res,next) => {
+    if(req.body.password1.length < 8) res.json({err: true, msg: 'Password too short, minimum length is 8 chars'});
+    else if(req.body.password1 !== req.body.password2) res.render('members/sign-up',{err: true, msg: 'Passwords don\'t match'});
+    else next();
+
+}
